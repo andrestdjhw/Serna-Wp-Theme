9927d0eaab53bc83bde35b8368dd67ac9f774ca3
andrestdjhw <46946159+andrestdjhw@users.noreply.github.com>
2025-04-29 13:35:56 -0600

    Centrando items del footer

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => '4f2628a8acfd0da28a5b');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => 'afd2e09af9fc2f968aea');
