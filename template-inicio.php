db38bc223dbb710412567f2012ae2fdfd7af48f3
andrestdjhw <46946159+andrestdjhw@users.noreply.github.com>
2025-12-12 11:49:44 -0600

    Agregando mapa interactivo en la pagina de Inicio SERNA

--- template-inicio.php
+++ template-inicio.php
@@ -370,6 +370,28 @@ get_header(); ?>
                 </a>
             </div>
         </section>
+
+        <!-- Sección para mapa de Google -->
+        <section class="mt-24">
+            <h2 class="text-3xl font-bold text-center mb-12 text-[#00903b]"><span class="bg-clip-text text-transparent bg-gradient-to-r from-[#00903b] to-[#7dbb5c]">Estamos aquí</span></h2>
+            <div class="bg-white rounded-lg shadow-custom overflow-hidden">
+                <div class="aspect-w-16 aspect-h-9">
+                    <!-- aquí va el frame del mapa de Google -->
+                    <iframe 
+                        src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d1707.8792146428075!2d-87.18747288964083!3d14.097344497602379!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e0!3m2!1ses-419!2shn!4v1747232723012!5m2!1ses-419!2shn" 
+                        width="100%" 
+                        height="600" 
+                        style="border:0;" 
+                        allowfullscreen="" 
+                        loading="lazy" 
+                        referrerpolicy="no-referrer-when-downgrade">
+                    </iframe>
+                    <!-- <div class="bg-gray-200 flex items-center justify-center" style="height: 400px;">
+                        <p class="text-gray-600">Mapa de ubicación</p>
+                    </div> -->
+                </div>
+            </div>
+        </section>
     </div>
 </div>
 
