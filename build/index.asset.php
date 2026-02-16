744bd791b8b2c8826bb8d6574e7e36fbf76bcce8
andrestdjhw <46946159+andrestdjhw@users.noreply.github.com>
2026-02-16 11:32:13 -0600

    Actualizando Footer efecto de Ola y Gradiante

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => '216df1d54ee762f52072');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => 'b7cb30e5cad568e63796');
